9bbbc9b6ddd58e7b426410e2796d2737aad11b95
Clinton Yade <yadeclinton20@gmail.com>
2025-10-27 23:51:28 +0300

    updated the authform

--- app/components/AuthForm.jsx
+++ app/components/AuthForm.jsx
@@ -86,7 +86,7 @@ export default function AuthForm() {
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
               required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              className="mt-1 block w-full px-3 py-2 border border-gray-500 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
             />
           </div>
         )}
--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@
 
 import './globals.css';
 import { Inter } from 'next/font/google';
-import Navbar from './components/Navbar'; // 💡 Import the new Navbar
+import Navbar from './components/Navbar'; 
 
 const inter = Inter({ subsets: ['latin'] });
 
